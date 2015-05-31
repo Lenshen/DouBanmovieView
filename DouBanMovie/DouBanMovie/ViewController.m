b4600b38e55bd1ccf51f2e890a9d7cfcc3de8db3
Lenshen <779822644@qq.com>
2015-05-31 16:02:42 +0800

    added object

---
+++ DouBanMovie/DouBanMovie/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  DouBanMovie
+//
+//  Created by 鲍先生 on 15/5/31.
+//  Copyright (c) 2015年 鲍先生. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+@property (weak, nonatomic) IBOutlet UITableView *tabView;
+
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
