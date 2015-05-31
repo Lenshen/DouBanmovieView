ea2ab020f5b71f96a25ce90c67b51645438daa5d
Lenshen <779822644@qq.com>
2015-05-31 17:28:39 +0800

    added cell 初始化ell类型

---
+++ DouBanMovie/DouBanMovie/DouBanTableViewCell.h
@@ -0,0 +1,13 @@
+//
+//  DouBanTableViewCell.h
+//  DouBanMovie
+//
+//  Created by 鲍先生 on 15/5/31.
+//  Copyright (c) 2015年 鲍先生. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DouBanTableViewCell : UITableViewCell
+
+@end
---
+++ DouBanMovie/DouBanMovie/DouBanTableViewCell.m
@@ -0,0 +1,23 @@
+//
+//  DouBanTableViewCell.m
+//  DouBanMovie
+//
+//  Created by 鲍先生 on 15/5/31.
+//  Copyright (c) 2015年 鲍先生. All rights reserved.
+//
+
+#import "DouBanTableViewCell.h"
+
+@implementation DouBanTableViewCell
+
+- (void)awakeFromNib {
+    // Initialization code
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
